780bfd028b69f4fd8a6c7d46db42ba396767093e
Renan Garcia <renanrgarcia@gmail.com>
2024-10-25 12:12:45 -0300

    Networks done

--- section4/mvc1/obj/Debug/net8.0/mvc1.AssemblyInfo.cs
+++ section4/mvc1/obj/Debug/net8.0/mvc1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7598abfbcf80efe8c0b60a910f9801c446c12f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6b49abb214d8f56cc8547ffba8009ad95f1ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
