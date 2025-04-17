505f7fab971ce8324f1b0d396584ca894caae325
Renan Garcia <renanrgarcia@gmail.com>
2025-04-17 08:15:59 -0300

    Add Discount.Grpc service integration to Basket.API with gRPC support and Docker configuration

--- section12/Microservices/Basket.API/Program.cs
+++ section12/Microservices/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<DiscountGrpcService>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!)
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
